69c89c678a2e8b010800305ce27b71e79528648f
John Allen <idealconstant2000@yahoo.com>
2025-10-03 14:28:30 -0500

    updates

--- ML_Python_ChatGPT/mathplotlib_1.py
+++ ML_Python_ChatGPT/mathplotlib_1.py
@@ -1,10 +1,8 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-import numpy as np
+arr = np.array([1, 3, 5, 7])
 
-arr = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9], [10, 11, 12], [13, 14, 15], [16, 17, 18]])
+x = np.searchsorted(arr, [2, 4, 6])
 
-newarr = np.hsplit(arr, 3)
-
-print(newarr)
+print(x)
